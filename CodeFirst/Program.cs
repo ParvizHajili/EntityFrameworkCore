24318bfaa3315e6c74b43b8dcf98714626777057
Parviz <parviz.hajili@gmail.com>
2022-08-21 14:43:24 +0400

    CodeFirst

---
+++ CodeFirst/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+using CodeFirst.Entities;
+using Microsoft.EntityFrameworkCore;
+Console.WriteLine("Hello World");
+
+
+
+//ExampleDbContext exampleDb = new();
+//await exampleDb.Database.MigrateAsync();
+
+public class ExampleDbContext : DbContext
+{
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Server = Localhost; Database = ExampleDb; Integrated Security = true;");
+    }
+    public DbSet<Product> Products { get; set; }
+    public DbSet<Customer> Customers { get; set; }
+}
