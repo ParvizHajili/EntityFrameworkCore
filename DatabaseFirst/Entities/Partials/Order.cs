9d3b62a578acf35151cb05d818e63be756392d6a
Parviz <parviz.hajili@gmail.com>
2022-08-21 13:25:26 +0400

    DbFirst

---
+++ DatabaseFirst/Entities/Partials/Order.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatabaseFirst.Entities
+{
+    public partial class Order
+    {
+        public int Example { get; set; }
+    }
+}
